618f96a254a4edd3e3813271f0abae4713ad9585
keikeime0828-bit <keikei.me0828@gmail.com>
2025-10-24 11:32:36 +0900

    Update anubis_alert.py

--- anubis_alert.py
+++ anubis_alert.py
@@ -1,14 +1,12 @@
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-# --- Slack設定 ---
-SLACK_TOKEN = "xoxb-XXXXXXXXXXXXXXXXXXXX"  # 自分のBotトークン
-SLACK_CHANNEL = "#anubis_alert"             # 通知先チャンネル
+SLACK_TOKEN = "xoxb-XXXXXXXXXXXXXXXXXXXX"
+SLACK_CHANNEL = "#anubis_alert"
 
 client = WebClient(token=SLACK_TOKEN)
 
-def send_alert(channel, message):
-    """Slackに通知"""
+def send_alert(message):
     try:
         client.chat_postMessage(channel=SLACK_CHANNEL, text=message)
     except SlackApiError as e:
